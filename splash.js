9f43eea5f531b6abba0eb85b35fa0449f7fb39a5
akshayasivajilal-code <akshayasivajilal@gmail.com>
2026-08-06 10:30:51 +0530

    Add files via upload

--- splash.js
+++ splash.js
@@ -1,22 +1,18 @@
-/* ===========================================
-          PAWHUB SPLASH SCREEN
-=========================================== */
 
-// Wait until page loads
 window.addEventListener("load", () => {
 
-    // Fade out animation before redirect
+ 
     const splash = document.querySelector(".splash-screen");
 
     setTimeout(() => {
 
         splash.style.opacity = "0";
         splash.style.transform = "scale(1.05)";
-        splash.style.transition = "all 0.8s ease";
+        splash.style.transition = "all 0.10s ease";
 
     }, 1800);
 
-    // Redirect to Home Page
+   
     setTimeout(() => {
 
         window.location.href = "home.html";
@@ -24,9 +20,6 @@ window.addEventListener("load", () => {
     }, 2600);
 
 });
-
-
-/* Optional: Skip splash by pressing Enter */
 
 document.addEventListener("keydown", (event) => {
 
@@ -38,11 +31,19 @@ document.addEventListener("keydown", (event) => {
 
 });
 
-
-/* Optional: Skip splash by clicking anywhere */
-
 document.addEventListener("click", () => {
 
     window.location.href = "home.html";
 
 });
+
+document.addEventListener("click",() => {
+
+    window.location.href = "login.html";
+{
+    setTimeout(function () {
+        window.location.href = "login.html";
+    }, 3000); 
+
+}  
+}); 
